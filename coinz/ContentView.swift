48037d7fda047034167d73c97b4c602ffd192c06
Tara Frye <tara.c.frye@gmail.com>
2021-09-28 16:13:25 -0400

    Initial Commit

---
+++ coinz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coinz
+//
+//  Created by Tara Frye on 9/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coinz/coinzApp.swift
@@ -0,0 +1,17 @@
+//
+//  coinzApp.swift
+//  coinz
+//
+//  Created by Tara Frye on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct coinzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
